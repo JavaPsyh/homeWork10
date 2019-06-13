d30846eadd142e4ed4ddda9d9d93739cabfcc535
JavaPsyh <psyhjava@gmail.com>
2019-06-13 10:03:33 +0300

    version for verification

--- src/my/hash/map/MyHashMap.java
+++ src/my/hash/map/MyHashMap.java
@@ -1,24 +1,20 @@
 package my.hash.map;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 public class MyHashMap<K, V> {
 
-
     private final static int DEFAULT_CAPACITY = 16;
     private final static double DEFAULT_LOAD_FACTOR = 0.75;
     private Node<K, V>[] arrayOfNodes;
-    private Node<K, V> first;
     private int size = 0;
-    public int newLength;
-
+    private int newLength;
 
     private void arrayGrower() {
         Node<K, V>[] temp;
         Node<K, V> tempNode;
         int newIndex;
-        if (arrayOfNodes == null){
+        if (arrayOfNodes == null) {
             arrayOfNodes = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
             newLength = DEFAULT_CAPACITY;
         } else {
@@ -30,9 +26,9 @@ public class MyHashMap<K, V> {
                     if (arrayOfNodes[i].nextNode == null) {
                         newIndex = newIndex(arrayOfNodes[i].key);
                         temp[newIndex] = arrayOfNodes[i];
-                    }else {
+                    } else {
                         tempNode = arrayOfNodes[i];
-                        while (tempNode.nextNode != null){
+                        while (tempNode.nextNode != null) {
                             newIndex = newIndex(tempNode.key);
                             temp[newIndex] = tempNode.nextNode;
                             tempNode = tempNode.nextNode;
@@ -51,19 +47,23 @@ public class MyHashMap<K, V> {
         int index = newIndex(newNode.key);
         if (arrayOfNodes[index] == null) {
             arrayOfNodes[index] = newNode;
-        }else {
-                temp = arrayOfNodes[index];
+        } else {
+            temp = arrayOfNodes[index];
+            if (key == null && temp.key == null){
+                temp.value = value;
+                return;
+            }
             if (key.equals(temp.key)) {
                 temp.value = value;
                 return;
             }
-                while (temp.nextNode != null){
-                    if (key.equals(temp.nextNode.key)) {
-                        temp.nextNode.value = value;
-                        return;
-                    }
-                    temp = temp.nextNode;
+            while (temp.nextNode != null) {
+                if (key.equals(temp.nextNode.key)) {
+                    temp.nextNode.value = value;
+                    return;
                 }
+                temp = temp.nextNode;
+            }
             newNode.nextNode = arrayOfNodes[index];
             arrayOfNodes[index] = newNode;
         }
@@ -76,18 +76,22 @@ public class MyHashMap<K, V> {
         V result = null;
         if (arrayOfNodes[indexKey] == null) {
             System.out.println("Not values with key: " + key);
-        }else {
+        } else {
             temp = arrayOfNodes[indexKey];
             if (temp.nextNode == null) {
                 result = temp.value;
             } else {
-                while (temp.nextNode != null){
+                while (temp.nextNode != null) {
+                    if (key == null && temp.key == null) {
+                        result = temp.value;
+                        break;
+                    }
                     if (key.equals(temp.key)) {
                         result = temp.value;
                         break;
                     }
                     temp = temp.nextNode;
-                    if (temp.nextNode == null){
+                    if (temp.nextNode == null) {
                         if (key.equals(temp.key)) {
                             result = temp.value;
                         }
@@ -109,35 +113,18 @@ public class MyHashMap<K, V> {
         private V value;
         private Node<K, V> nextNode = null;
 
-        public Node(K key, V value) {
+        private Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
-
-        public Node(K key, V value, Node<K, V> nextNode) {
-            this.key = key;
-            this.value = value;
-            this.nextNode = nextNode;
-        }
-
-        @Override
-        public  boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Node<?, ?> node = (Node<?, ?>) o;
-            return Objects.equals(key, node.key);
-        }
-
-
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        final int answer = 42;
-        int result = answer * prime;
-        result = prime * first.key.hashCode() / answer + result;
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?, ?> node = (Node<?, ?>) o;
+        return Objects.equals(this, node.key);
     }
 
     private <K> int hash(K key) {
@@ -145,9 +132,9 @@ public class MyHashMap<K, V> {
         int hashOfKey = key.hashCode();
         return (hashOfKey ^ (hashOfKey >>> 16));
     }
-    public int hashIndex;
-    private <K> int newIndex (K key){
-        hashIndex = (newLength - 1) & (hash(key));
-    return hashIndex;
+
+    private <K> int newIndex(K key) {
+        int hashIndex = (newLength - 1) & (hash(key));
+        return hashIndex;
     }
 }
